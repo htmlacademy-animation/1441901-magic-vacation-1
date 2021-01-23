01a92d38f4a8472575005427c06b8c82f10e87f5
helen <helen@denis.lobach>
2021-01-23 19:05:23 +0300

    module4-task2

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,48 +1,40 @@
 export default () => {
-  const prizeScreen = document.querySelector(`.screen--prizes`);
-  const prizeJourneys = prizeScreen.querySelector(`.prizes__item--journeys img`);
-  const prizeCases = prizeScreen.querySelector(`.prizes__item--cases img`);
-  const prizeCodes = prizeScreen.querySelector(`.prizes__item--codes img`);
-
-  const images = [
-    {
-      name: `prize-journeys`,
-      path: `img/prize1.svg`,
-      timeDelay: 500,
-      target: prizeJourneys,
-    },
-    {
-      name: `prize-cases`,
-      path: `img/prize2.svg`,
-      timeDelay: 500,
-      target: prizeCases,
-    },
-    {
-      name: `prize-codes`,
-      path: `img/prize3.svg`,
-      timeDelay: 500,
-      target: prizeCodes,
-    },
+    const prizeScreen = document.querySelector(`.screen--prizes`);
+  const prizes = document.querySelectorAll(`.prizes__item`);
+  const prizesImg = document.querySelectorAll(`.prizes__item img`);
+  const prizesNum = document.querySelectorAll(`.prizes__desc b`);
+  const prizesOptions = [
+    {src: `img/prize1.svg`, timeout: 500, numbers: [1, 2, 3]},
+    {src: `img/prize2.svg`, timeout: 1000, numbers: [1, 2, 3, 4, 5, 6, 7]},
+    {src: `img/prize3.svg`, timeout: 1500, numbers: [11, 121, 216, 324, 419, 513, 628, 734, 826, 875, 900]}
   ];
 
-  
-  function addImagesSvg() {
-    if (!prizeJourneys.hasAttribute(`src`)) {
-      images.forEach(({path, timeDelay, target, name}, i) => {
-        setTimeout(() => {
-          target.setAttribute(`src`, `${path}?time=${Date.now()}`);
 
-          
-          if (target === prizeJourneys) {
-            prizeScreen
-              .querySelector(`.prizes__item--journeys`)
-              .classList.add(`active`);
-          }
-        }, timeDelay);
-      });
+  function drawNums(prizeNum, prizesOption, fpsInterval, now, then, nums, elapsed) {
+    requestAnimationFrame(() => drawNums(prizeNum, prizesOption, fpsInterval, now, then, nums, elapsed));
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval && nums.length > 0) {
+      then = now - (elapsed % fpsInterval);
+      prizeNum.textContent = nums.shift();
     }
-    return;
   }
+
+  function addImagesSvg(){
+    for (let i = 0; i < prizesImg.length; i++) {
+      let fpsInterval = 1000 / 12;
+      let now = Date.now();
+      let then = Date.now();
+      let elapsed;
+      setTimeout(() => {
+        prizes[i].classList.add(`prizes__item--active`);
+        prizesImg[i].src=prizesOptions[i].src;
+        requestAnimationFrame(() => drawNums(prizesNum[i], prizesOptions[i], fpsInterval, now, then, prizesOptions[i].numbers, elapsed));
+      }, prizesOptions[i].timeout);
+    }
+  }
+
 
   if (prizeScreen.classList.contains(`active`)) {
     addImagesSvg();
@@ -53,5 +45,8 @@ export default () => {
       addImagesSvg();
     }
   });
+  
+
+
 
 };
