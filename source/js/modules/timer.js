7b29ffd27df98c7a1ff7d8a1c48159bdfa8093a5
helen <helen@denis.lobach>
2021-01-23 15:20:55 +0300

    module4-task1 правки

--- source/js/modules/timer.js
+++ source/js/modules/timer.js
@@ -4,30 +4,41 @@ export default () => {
   const minutesElement = document.querySelector(`.game__counter span:first-child`);
   const secondsElement = document.querySelector(`.game__counter span:last-child`);
 
+  let fpsInterval = 1000 / 12;
+  let now, then = Date.now(), elapsed;
+
   function showTime() {
     let currTime = Date.now(); 
     let minutes='00';
-	let seconds='00';
-	let timeDiff=endTime-currTime;
-	if(timeDiff>0){
-		let timer=new Date(timeDiff); 
-		minutes=timer.getMinutes()<10 ? "0"+timer.getMinutes() : timer.getMinutes();  
-		seconds=timer.getSeconds()<10 ? "0"+timer.getSeconds() : timer.getSeconds();  
-	}
+    let seconds='00';
+    let timeDiff=endTime-currTime;
+    if(timeDiff>0){
+      let timer=new Date(timeDiff); 
+      minutes=timer.getMinutes()<10 ? "0"+timer.getMinutes() : timer.getMinutes();  
+      seconds=timer.getSeconds()<10 ? "0"+timer.getSeconds() : timer.getSeconds();  
+    }
     minutesElement.textContent = minutes;
     secondsElement.textContent = seconds;
-	requestAnimationFrame(showTime);
+  }
+
+  function tick() {
+    requestAnimationFrame(tick);
+    now = Date.now();
+    elapsed = now - then;
+    if (elapsed > fpsInterval) {
+      then = now - (elapsed % fpsInterval);
+      showTime();
+    }
   }
 
   window.addEventListener(`popstate`, function(){
-	endTime = Date.now()+game_duration;
-	showTime();
-  }, false);
-  
-  window.addEventListener(`load`, function(){
-	endTime = Date.now()+game_duration;
-	showTime();
+    endTime = Date.now()+game_duration;
+    tick();
   }, false);
 
+  window.addEventListener(`load`, function(){
+    endTime = Date.now()+game_duration;
+    tick();
+  }, false);
 };  
 
